a5978286ce4cb39561ded8d8ece2db7a9365a36a
venemexcoin <venemexcoin@gmail.com>
2021-03-02 07:50:16 -0600

    commit pausa 02/03/2021

--- nuevaPrograma/pruebasForForeachVenCoin/compra.js
+++ nuevaPrograma/pruebasForForeachVenCoin/compra.js
@@ -14,6 +14,9 @@ const campoC = {
 }
 
 const compra = []
+const valorRest = []
+
+const porsentaje = 0.0050
 
 const validarFormularioCompra = (e) => {
     //console.log(e.target.value)
@@ -64,6 +67,7 @@ formCompra.addEventListener('submit', (e) => {
     
     if(campoC.presioC && campoC.valorC ){
         formCompra.reset()
+        ConpraValida()
         document.getElementById('formulario__mensajeCompra-exito').classList.add('formulario__mensajeCompra-exito-activo')
 
         setTimeout(() => {
@@ -77,3 +81,75 @@ formCompra.addEventListener('submit', (e) => {
         },5000)
     }
 })
+
+
+const ConpraValida = () => {
+
+    // operacion cuando compras BTC con pesos 
+    const number4 = compra.presioC     //compra de cripto precio en fias
+    const number5 = compra.valorC   //precio del mercado al comprar
+    const number6 = 0.0050
+    
+    const res4 = number4 / number5 // monto comprado/posentage
+    const res5 = res4 * number6  //comicsion
+    const res6 = res4 - res5     // cuanto te dan
+
+    valorRest ["res4"] = res4
+    valorRest ["number4"] = number4
+
+    console.log(res6)
+    //fin de esta operacion pero se puede unir con la sigiente directamente al poner el monto 
+
+}
+
+ // operacion de ganacia 
+ const number7 = 951853.00 // este monto en pesos es al que quieres vender 
+ const number8 = porsentaje
+ const number9 = valorRest.res4  //monto que yo deveria tener
+ 
+ const number10 =  number9 - number8 //diferencia entre compara y lo que dan
+ const number11 =  number10 + number8 // monto para estar en el monto que tenia que tener
+
+// si yo vendo
+
+const venta = number11
+
+const porcetaje = 0.018
+const valorPorce = number7 * porcetaje
+const valor2 = number7 + valorPorce  // monto de venta
+
+const valor3 = number11 * valor2  //monto de recuperacion
+
+/* -----------------------------------------------------*/
+
+
+// operacion cuando compras BTC con pesos 
+const number12 = valor3
+const number13 = valor2
+const number14 = 0.0065
+
+const res12 = number12 / number13 // monto comprado/porsentage
+const res13 = res12 * number14  //comicsion
+const res14 = res12 - res13     // cuanto te dan
+
+
+/* ------------------------------------------------------------*/
+
+const ganacia1 = res14  // monto de recuperacion
+const comicion = 0.0065
+
+const resulganacia = ganacia1 * comicion  //retamos la comicion
+const ganacia2 = ganacia1 + resulganacia  //incrementamos la comision
+
+const ganacia3 = valor2 * comicion  //incremento para optener ganacia
+
+const ganacia4 = valor2 + ganacia3 //este es el precio para la veta con ganacia
+
+
+/* -----------------------------------------------------------*/
+
+//cuanto me gane
+
+const ganacia5 = ganacia1 * ganacia4 
+
+const ganaciatotal = ganacia5 - valorRest.number4
