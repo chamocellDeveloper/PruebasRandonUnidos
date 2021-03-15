cca49a5340417da1d675d9c6d9b3d5b9e7cc56d2
chamocellDeveloper <chamocelDeveloper@gimail.com>
2021-03-15 12:56:54 -0600

    commit pactica de video

--- ProgramandoEl Destino/ApiVideo/app.js
+++ ProgramandoEl Destino/ApiVideo/app.js
@@ -1,3 +1,15 @@
+function getMinutesAndSeconds(time) {
+    let minutes = Math.floor(time / 60)
+    let segunds = Math.floor(time % 60)
+
+    return `${minutes}: ${segunds}`
+
+    if(minutes < 10 ) minutes = `0s{minutes}`
+
+    if(seconds < 10 ) minutes = `0s{seconds}`
+}
+
+
 const video = document.querySelector('video')
 const play = document.getElementById('play')
 const pause = document.getElementById('pause')
@@ -5,7 +17,18 @@ const full = document.getElementById('full')
 const volume = document.getElementById('volume')
 const time = document.getElementById('time')
 const pip = document.getElementById('pip')
+const mute = document.getElementById('mute')
 
+const current = document.getElementById('current')
+const duration = document.getElementById('duration')
+
+duration.textContent = getMinutesAndSeconds(video.duration)
+console.log(video.duration)
+
+video.addEventListener('loadedmetadata', e => {
+    duration.textContent = getMinutesAndSeconds(video.duration)
+    console.log(video.duration)
+})
 
 play.addEventListener('click', (e) => {
     video.play()
@@ -25,6 +48,8 @@ volume.addEventListener('change', (e) => {
 
 video.addEventListener('timeupdate', (e) => {
     time.value = Math.floor(video.currentTime * 100 / video.duration)
+
+    current.textContent = getMinutesAndSeconds(video.currentTime)
 })
 
 time.addEventListener('input', (e) => {
@@ -34,4 +59,13 @@ time.addEventListener('input', (e) => {
 pip.addEventListener('click', (e) => {
     video.requestPictureInPicture()
     video.style.display = "none"
+})
+
+video.addEventListener('leavepictureinpicture', e => {
+    video.style.display = 'block'
+})
+
+mute.addEventListener('click', e => {
+    video.muted = !video.muted
+    mute.textContent = video.muted ? 'unmute' : 'Mute'
 })
